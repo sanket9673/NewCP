180aadf7b261cea46b5dfabe2ec829b47d4ab7d9
sanket9673 <sanketch9673@gmail.com>
2025-08-21 00:45:32 +0530

    Number of Zero Filled subarrys

---
+++ LC_2348.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long count = 0, res = 0 ;
+        for (int x : nums) {
+            if (x == 0) count++ ;
+            else {
+                res += count ;
+                res += (count*(count-1)) / 2 ;
+                count = 0 ;
+            }
+        }  
+        if (count > 0) {
+            res += count ;
+            res += (count*(count-1)) / 2 ;
+        }
+        return res ; 
+    }
+};
