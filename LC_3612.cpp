bfe851a39b93043698c7abdc72f881b214302402
sanket9673 <sanketch9673@gmail.com>
2025-09-17 01:07:57 +0530

    Process string with the special operation I

---
+++ LC_3612.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        // given s consists of lower and special charaters
+
+        // handle edge cases like "#*%*#%#"
+        string result = "" ;
+        unordered_set<char> check = {'#', '%', '*'} ;
+        for (char c : s) {
+            if (!result.empty()) {
+                if (c == '*') result.pop_back() ;
+                else if (c == '#') result += result ;
+                else if (c == '%') reverse(result.begin(), result.end()) ;
+                else result += c ;
+            }
+            else {
+                if (check.count(c) == 0) result += c ;
+            }
+        }
+        return result ;
+    }
+};
