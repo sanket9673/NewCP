e9285d12d52834dbd145dd39a8dc99e87ba1c867
sanket9673 <sanketch9673@gmail.com>
2026-01-08 10:36:08 +0530

    Capacity to ship D packages

---
+++ LC_1011.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canship(vector<int>& weights, int days, int w) {
+        int pack = 0, useday = 1 ;
+        for (int x : weights) {
+            if (x > w) return false ;
+            if (pack + x <= w) pack += x ;
+            else {
+                useday++ ;
+                pack = x ;
+            }
+        }
+        return (useday <= days) ? true : false ;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        // BINARY SERACH ON ANSWER
+        // order in the weight array matters
+        int low = *max_element(weights.begin(), weights.end()) ;
+        int high = 0 ;
+        for (int x : weights) high += x ;
+        int result = INT_MAX ;
+        while (low <= high) {
+            int mid = low + (high - low) / 2 ;
+            bool answer = canship(weights, days, mid) ;
+            if (answer == true) {
+                result = min(result, mid) ;
+                // now we search is there any more small value possible ?
+                high = mid - 1 ;
+            }
+            else low = mid + 1 ;
+        }
+        return result ;
+    }
+};
