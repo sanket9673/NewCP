5f8b71a92387a369eaad113d2dbde036e0fa28c9
sanket9673 <sanketch9673@gmail.com>
2026-01-13 11:36:32 +0530

    Segrregate odd and even nodes in LL

--- LC_328.cpp
+++ LC_328.cpp
@@ -27,33 +27,28 @@ public:
         odd -> next = evenhead ;
         return head ;
 
-        // FRAGILE CODE (Bugs present)
+        // FRAGILE CODE
         // if (head == nullptr || head -> next == nullptr) return head ;
-        // ListNode* temp1 = head -> next ;
-        // ListNode* prev = head ;
+        // ListNode* temp1 = head ;
         // int sz = 1 ;
         // while (temp1 -> next != nullptr) {
-        //     prev = prev -> next ;
         //     temp1 = temp1 -> next ;
         //     sz++ ;
         // }
         // ListNode* temp = head ;
-        // ListNode* last ;
-        // bool evenlength = false ;
-        // if (sz % 2 == 0) {
-        //     sz = sz/2 - 1 ; 
-        //     last = prev;
-        //     evenlength = true ;
-        // }
-        // else {sz = sz/2 ; last = temp1;}
-        // for (int i = 0 ; i <= sz ; i++) {
+        // ListNode* last = temp1 ;
+        // for (int i = 0 ; i < sz/2 ; i++) {
+        //     if (temp -> next == last) break ;
+
         //     ListNode* mov = temp -> next ;
         //     temp -> next = mov -> next ;
         //     mov -> next = nullptr ;
+
         //     last -> next = mov ;
-        //     last = last -> next ;
+        //     last = mov;
+            
+        //     temp = temp -> next ;
         // }
-        // if (evenlength == true) last -> next = temp1 ;
         // return head ;
     }
 };
