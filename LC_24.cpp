472666c09f9b214777f70eb015620705fb8f9d50
sanket9673 <sanketch9673@gmail.com>
2026-02-18 02:37:57 +0530

    Swap Nodes

---
+++ LC_24.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head -> next == nullptr) return head ; 
+        ListNode* dummy = new ListNode(0) ;
+        dummy -> next = head ;
+        ListNode* prev = dummy ;
+        while (prev -> next != nullptr && prev -> next -> next != nullptr) {
+            ListNode* first = prev -> next ;
+            ListNode* second = first -> next ;
+            // swapping
+            first -> next = second -> next ;
+            second -> next = first ;
+            prev -> next = second ;
+            // move prev forward for next pointer
+            prev = first ;
+        }
+        return dummy -> next ;
+    }
+};
