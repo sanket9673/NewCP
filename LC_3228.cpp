c05ac2e0711698d2393272dd5258e14828efe30f
sanket9673 <sanketch9673@gmail.com>
2026-02-20 00:48:56 +0530

    Max number of operations to move ones to the end

--- LC_3228.cpp
+++ LC_3228.cpp
@@ -4,6 +4,7 @@ public:
         int ans = 0, one = 0 ;
         bool ch = false ;
         // zero single - no : zeros treated as one block and 1 can pass as block togethe of 0
+        // 1 can cross 00 at one in one stepp therefore BLOCK CONSIDERED
         for (int i = 0; i < s.length(); i++) {
             if (s[i] == '1') {one++ ; ch = false ;}
             else {
