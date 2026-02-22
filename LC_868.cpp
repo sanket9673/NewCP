a0fec5511d01916f9e7b0f5fdfe3a59a1e841d28
sanket9673 <sanketch9673@gmail.com>
2026-02-22 21:21:55 +0530

    Binary grap

---
+++ LC_868.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        string res = "" ;
+        while (n > 0) {
+            int bit = n & 1 ;
+            res = to_string(bit) + res ;
+            n >>= 1 ;
+        }
+        vector<int> rest ;
+        for (int i = 0; i < res.size(); i++) {
+            if (res[i] == '1') rest.push_back(i) ;
+        }
+        if (rest.size() == 1) return 0 ;
+        int ans = 0; 
+        for (int i = 0; i < rest.size() - 1; i++) {
+            ans = max(ans, rest[i+1] - rest[i]) ;
+        }
+        return ans ;
+    }
+};
